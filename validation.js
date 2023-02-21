68cdc3749277cc857ec34a14aa2c0ae745b5722f
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-02-21 18:49:29 +0530

    Delete validation.js

--- validation.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-toggle").addEventListener("click", function() {
-  var menu = document.querySelector("header nav ul");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
-});
